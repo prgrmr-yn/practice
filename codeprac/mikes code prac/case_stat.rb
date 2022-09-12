0a0ba96c415aa489290e8f8e85e8c0b1c10dbbc9
Yatin Nagpal <nnega4428@gmail.com>
2022-09-12 18:23:23 +1000

    i like this one better

---
+++ codeprac/mikes code prac/case_stat.rb
@@ -0,0 +1,14 @@
+def get_day_name(day)
+  case day
+  when 'mon' then 'monday'
+  when 'tues', 'tue' then 'tuesday'
+  when 'wed' then 'wednesday'
+  when 'thu', 'thurs' then 'thursday'
+  when 'fri' then 'friday'
+  when 'sat' then 'saturday'
+  when 'sun' then 'sunday'
+  else 'Invalid  abbreviation of day'
+  end
+end
+
+puts get_day_name 'thu'
