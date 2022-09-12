69e4de3af6e1c906fa6abcb41f175f7b696f391d
Yatin Nagpal <nnega4428@gmail.com>
2022-09-12 17:36:09 +1000

    ARGV practice

---
+++ codeprac/others/max_method.rb
@@ -0,0 +1,16 @@
+# def max(num1, num2, num3)
+#   if num1 >= num2 && num1 >= num3
+#     num1
+#   elsif num2 >= num1 && num2 >= num3
+#     num2
+#   else
+#     num3
+#   end
+# end
+
+# puts max(ARGV[0], ARGV[1], ARGV[2])
+
+def greeting name
+  name.upcase
+end
+puts greeting ARGV.first
