383ae2574e5dcb51455e87b516947896a48afb40
Yatin Nagpal <nnega4428@gmail.com>
2022-09-12 15:23:06 +1000

    fun excercise

---
+++ codeprac/hashes.rb/zipcode.rb
@@ -0,0 +1,14 @@
+zip_code = {
+  dandenong: 3175,
+  eummemoring: 3176,
+  carnegie: 3163,
+  bayswater: 3163,
+  fitzroy: 3065,
+}
+zip_code.default = 'unknown suburb, please try again'
+
+
+puts 'whats the postcode you wanna know'
+key = gets.chomp.gsub(' ','_')
+puts key
+puts "0#{zip_code[key.to_sym]}" if zip_code.key?(key)
