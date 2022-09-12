47829437248c7a4dc2bf4eebd103c0a2219655bb
Yatin Nagpal <nnega4428@gmail.com>
2022-09-12 15:58:04 +1000

    nice game

---
+++ codeprac/others/mad_lib.rb
@@ -0,0 +1,17 @@
+# mad libs game
+
+
+def inp
+  gets.chomp
+end
+
+puts 'Enter a color'
+color = inp
+puts 'Enter an plural noun'
+plural_noun = inp
+puts 'Celebrity'
+celebrity = inp
+
+puts "Roses are #{color}"
+puts "#{plural_noun} are blue"
+puts "I love #{celebrity}"
--- codeprac/others/strings.rb
+++
@@ -1,4 +0,0 @@
-num = 20.39
-string = 'hello'
-
-puts "#{Math.sqrt(4).to_i} is #{string}"
