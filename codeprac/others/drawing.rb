8b31a15e1f894f94287c8f4b1c13d187f3086804
Yatin Nagpal <nnega4428@gmail.com>
2022-09-12 15:24:31 +1000

    nice drawing

---
+++ codeprac/others/drawing.rb
@@ -0,0 +1,13 @@
+puts '    /\\'
+puts '   /  \\'
+puts '  /____\\'
+puts ' /      \\'
+puts '/        \\'
+
+
+puts '|¯¯\\'
+puts '|   \\'
+puts '|___/'
+puts '|   \\'
+puts '|    \\'
+puts '|____/'
