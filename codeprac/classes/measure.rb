ec1e8a552805c3fdac151c397f65e4c582286813
Yatin Nagpal <nnega4428@gmail.com>
2022-09-12 12:32:26 +1000

    sharping my skills

--- codeprac/classes/measure.rb
+++ codeprac/classes/measure.rb
@@ -1,7 +1,9 @@
 class Box
+  @@num_of_boxes = 0
   def initialize(height, width)
     @height = height
     @width = width
+    @@num_of_boxes += 1
   end
 
   def get_height
@@ -24,9 +26,20 @@ class Box
     @height = h
   end
 
-  def getArea
+  def get_area
     @height * @width
   end
+
+  def self.full_boxes
+    "packed boxes till this date is #{@@num_of_boxes} boxes"
+  end
+
 end
 
-p Box.new
+box = Box.new(20, 23)
+box1 = Box.new(30, 23)
+box2 = Box.new(200, 23)
+p [box, box1, box2]
+
+p Box.full_boxes
+p box.get_area
