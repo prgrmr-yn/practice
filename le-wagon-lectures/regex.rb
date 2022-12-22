3a7dbee003b7a4951e5f2483a73cd8ca4d9116bb
Yatin Nagpal <nnega4428@gmail.com>
2022-12-22 19:21:36 +1100

    revised regex successfully

--- le-wagon-lectures/regex.rb
+++ le-wagon-lectures/regex.rb
@@ -1,8 +1,28 @@
 puts  'please tell me the email'
 
-email= gets.chomp
+email= 'yy@gaming.com'
 if email.match /\w+\@\w+\.\w{2,}/
-  puts 'its an email'
+  'its an email'
 else
-  puts 'not an valid email'
+  'not an valid email'
 end
+
+name_pattern = /(?<first_name>\w+) (?<last_name>\w+)/i
+
+'What is your name'
+name = 'anna ferrer'
+
+if name.match(name_pattern)
+  match_data = name.match(name_pattern)
+  "your first name is #{match_data[:first_name]} and your last name is #{match_data[:last_name]}"
+end
+
+# gsub
+
+'wassup boys'.gsub(
+  /^(\w+) (\w+)$/, 'Hey \2, \1'
+)
+
+p 'my hobby is a programmer'.gsub(
+   /(\w+) (\w+) (\w+) (\w+) (\w+)/, 'Hey \2 \1 \3 \4\5'
+)
