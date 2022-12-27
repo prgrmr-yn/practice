56346e3386b172015c54b31e6769ca18d4b9601f
Yatin Nagpal <nnega4428@gmail.com>
2022-12-28 10:33:51 +1100

    just adding more parsing using differnt methods

---
+++ codeprac/backend/ruby/parsing/api_using_rest_client.rb
@@ -0,0 +1,10 @@
+require 'rest-client'
+require 'json'
+
+url = "https://api.github.com/users/prgrmr-yn/repos"
+response = RestClient.get(url)
+repos = JSON.parse(response)
+
+# repos.each do |repo|
+#   Restaurant.create(name: repo['name'], rating: rand(1..5), address: repo['name'])
+# end
