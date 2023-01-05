7e159d60f92812b2cda475bdc0f52d8c383e3755
Yatin Nagpal <nnega4428@gmail.com>
2023-01-05 11:23:05 +1100

    Started a new js video series with freeCodeCamp(scrimba.com) and doing a 8 hour tutorial

---
+++ codeprac/backend/js/fccScrimba/index.js
@@ -0,0 +1,18 @@
+//Exercise 1
+// create two variables, myAge and humanDogRatio
+// Multiply the two together and store the result in myDogAge
+// Log myDogAge to the console
+
+const myAge = 25;
+const humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio;
+myDogAge;
+
+// Excercise 2
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100
+// Decrease it down to 25, and then finally increase it to 70
+
+let bonusPoints = 50;
+bonusPoints += 50;
+bonusPoints -= 75;
+bonusPoints += 45;
