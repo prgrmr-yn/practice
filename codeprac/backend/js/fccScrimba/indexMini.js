c9453ed1e1106c442ea65b8a773b9b1c6c213fd2
Yatin Nagpal <nnega4428@gmail.com>
2023-01-05 15:11:50 +1100

    finished with two mini excercises and wrapped it inside div and hide() to save pace to do more w/o getting confused

---
+++ codeprac/backend/js/fccScrimba/indexMini.js
@@ -0,0 +1,55 @@
+function hide(params) {
+  // const welcomeEl = document.getElementById('welcome-el')
+
+// const name = 'Per'
+// const greeting = 'Welcome Back'
+
+// welcomeEl.innerText = greeting + ', ' + name
+
+// console.log(name + '');
+
+// let errPara = document.getElementById('error')
+
+// function purchase() {
+//   console.log('he');
+//   errPara.textContent = 'Something went wrong, Please try again';
+// }
+
+//------------2---------------
+
+// const num1 =5;
+// const num2 =2;
+
+// document.getElementById('num1-el').textContent = num1;
+// document.getElementById('num2-el').textContent = num2;
+// const result = document.getElementById('el')
+
+
+// function show(calc) {
+//   result.textContent ='Result: ' +  calc
+// }
+
+// function add() {
+//   let calc =  num1 + num2
+//   show(calc)
+// }
+
+// function sub() {
+//   let calc =  num1 - num2
+//   show(calc)
+// }
+
+// function mul() {
+//   let calc =   num1 * num2
+//   show(calc)
+// }
+
+// function div() {
+//   let calc =   num1 / num2
+//   show(calc)
+
+// }
+
+}
+
+
