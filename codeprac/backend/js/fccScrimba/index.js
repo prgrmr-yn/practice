c99d34901e5ac5bfc178c1095acffa320035cda9
Yatin Nagpal <nnega4428@gmail.com>
2023-01-05 13:54:25 +1100

    created a people count app the increments decrements and reset, working on save functionality

--- codeprac/backend/js/fccScrimba/index.js
+++ codeprac/backend/js/fccScrimba/index.js
@@ -16,3 +16,63 @@ let bonusPoints = 50;
 bonusPoints += 50;
 bonusPoints -= 75;
 bonusPoints += 45;
+
+
+// Excercise 3
+// People counter
+// initialize the count as zero
+// listen for clicks
+// increment on clicks
+// show on html
+let count = 0;
+const incButton = document.getElementById('count-el');
+
+function showCount() {
+  incButton.innerText = count;
+}
+
+function resett() {
+  count = 0
+  showCount()
+}
+
+function increment() {
+  count++;
+  showCount()
+}
+
+function decrement() {
+  count--
+  if (incButton.innerText <= 0) resett();
+  showCount()
+}
+
+function save() {
+  console.log(count);
+}
+
+
+// Exercise
+// Concatenate the string using messed up method
+let username = 'per'
+let message = 'You have three notifications'
+let messageToUser = message + ',  ' +username + '!'
+
+
+//Excercise 4
+// Lap count
+
+let lapsCompleted = 0
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function incrementLap() {
+  lapsCompleted++
+}
+
+incrementLap()
+incrementLap()
+
+lapsCompleted; // console.log();
+
+incButton.draggable = true
