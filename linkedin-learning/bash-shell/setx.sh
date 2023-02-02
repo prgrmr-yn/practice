0f773821cde39538be158a22b76f44e4ead9b602
Yatin Nagpal <nnega4428@gmail.com>
2023-02-02 21:10:35 +1100

    done for the day

---
+++ linkedin-learning/bash-shell/setx.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# x=22
+
+
+# echo "Hello World"
+# echo -n Good to see you "\n\n"
+# echo Thanks
+# echo -e "HI \t\t\t There \n\n\n"
+# echo -E "BYE \t\t\t For Now \n\n\n"
+
+
+# # declare -l lstring="AFSD"
+
+# lc(){
+#     case "$1" in
+#         [A-Z])
+#         n=$(printf "%d" "'$1")
+#         n=$((n+32))
+#         printf \\$(printf "%o" "$n")
+#         ;;
+#         *)
+#         printf "%s" "$1"
+#         ;;
+#     esac
+# }
+# word="I Love Bash"
+# for((i=0;i<${#word};i++))
+# do
+#     ch="${word:$i:1}"
+#     lc "$ch"
+# done
+
+# ls -l
+
+
+# for i in dog cat elephant
+# do
+#     sleep 0.5
+#     echo i is $i
+# done
+
+
+# for num in `seq 1 5`
+# do
+#     echo $num
+#     sleep 0.4
+# done
+
+
+# for al in {a..z}
+# do
+#     echo $al
+# done
+
+
+# read a  <./setx.sh
+# echo $a
+
+# bash --version
+
+curdir=$(pwd)
+
+ls >> $curdir/list.txt
+ls /coolies 1>>list.txt 2>>error.txt
+
+
+# Junior Software Developor with background in warehousing and management
+# Recently graduated from Le Wagon intensive Web Development Bootcamp and completed a group project using HTML, CSS, JS AND RUBY ON RAILS,
+# I am looking for opportunities in Backend Development as i really enjoy writing short scrits to make my life easy using code and automating tasks
