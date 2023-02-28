04418f569414b0721ef97cdd7138e12255d30f75
Yatin Nagpal <nnega4428@gmail.com>
2023-03-01 06:28:18 +1100

    done for today

--- codeprac/youtubers/nerds-lesson/javascript/main2.js
+++ codeprac/youtubers/nerds-lesson/javascript/main2.js
@@ -16,3 +16,36 @@ function chapOne() {
 document.addEventListener('keydown', (e) => {
     console.log(`The ${e.key} key was pressed`)
 })
+
+
+
+newVar = 'not properly defined';
+console.log(newVar);
+
+for (let i = 0; i<10; i++) {
+  console.log(i);
+}
+
+let myImages = ['../images/img1.PNG', '../images/img2.PNG', '../images/img3.jpg'];
+
+let currentImage = 0;
+document.getElementById('next').onclick = nextPhoto;
+document.getElementById('previous').onclick = previousPhoto;
+const imgEl = document.getElementById('myImage')
+
+
+function nextPhoto() {
+  currentImage++
+  if (currentImage === myImages.length) {
+    currentImage = 0
+  }
+  imgEl.src = myImages[currentImage]
+}
+
+function previousPhoto() {
+  currentImage--
+  if (currentImage < 0){
+    currentImage = myImages.length - 1
+  }
+  imgEl.src = myImages[currentImage]
+}
